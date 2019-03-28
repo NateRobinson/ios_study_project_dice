33f557c2297d7fe66d145821d9bdd50795baa2fe
NateRobinson <840501291@qq.com>
2019-03-28 18:40:07 +0800

    finish demo code

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6",]
 
+    @IBOutlet weak var diceImg1: UIImageView!
+    @IBOutlet weak var diceImg2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateDiceImages()
     }
 
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages(){
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImg1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImg2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
 }
 
