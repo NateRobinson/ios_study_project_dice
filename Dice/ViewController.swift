85d7f706a22efeda18f1daf024c6993d480b2f27
NateRobinson <840501291@qq.com>
2019-03-28 17:26:59 +0800

    Initial Commit

---
+++ Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by  NateRobinson on 2019/3/28.
+//  Copyright © 2019  NateRobinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
